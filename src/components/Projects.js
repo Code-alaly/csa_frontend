fc2c55af0da9c5e35ba6dbaef7aa675222d98b5b
Code-alaly <dubiszd@gmail.com>
2021-10-31 15:37:32 -0700

    I'm super lost on how to actually like, get this flow going. I'm going to step away for a bit.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,17 +2,25 @@ import React, {useState, useEffect} from "react";
 import AuthService from "../services/auth.service";
 import authHeader from "../services/auth-header";
 import Project from "../services/auth.project"
+import projectTile from "../components/ProjectTile";
+import data from "bootstrap/js/src/dom/data";
 
 
 const Projects = () => {
     //this will now just be static
 
     const [content, setContent] = useState("");
+    const [show, setShow] = useState(undefined)
 
     useEffect(() => {
         Project.getProjects().then(
             (response) => {
-                setContent(response.data);
+                if (!Array.isArray(response.data)) {
+                    setShow(response.data);
+                } else {
+                    setContent(response.data)
+                }
+
             },
             (error) => {
                 const _content =
@@ -28,12 +36,22 @@ const Projects = () => {
     }, []);
 
     return (
-        <div className="container">
-            <header className="jumbotron">
-                <h3>{content}</h3>
-            </header>
-        </div>
-    );
+        <div>
+            {show ? (
+            <div className="container">
+                <header className="jumbotron">
+                    <h3>{show}</h3>
+                </header>
+            </div>
+            ) : (
+            <div className="container">
+                <header className="jumbotron">
+                    {projectTile}
+                </header>
+            </div>
+            )}
+
+        </div>);
 };
 
 export default Projects;
